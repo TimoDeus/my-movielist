b476744d2d45d15800cad139e8ce772d68daaa2d
Timo Deuschle <timo.deuschle@kaiserkraft-europa.de>
2018-09-05 17:29:49 +0200

    custom click handler for genre, director and actor labels

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Card, Icon, Image, Label} from 'semantic-ui-react'
+import * as PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
 
-const printLabels = input =>
-	input.split(',').map((actor, idx) => <Label key={idx} size='tiny' content={actor.trim()}/>);
+const printLabels = (input, clickHandler) =>
+	input.split(',')
+	.map(s => s.trim())
+	.map((value, idx) =>
+		<Label key={idx} size='tiny' content={value} onClick={clickHandler(value)}/>
+	);
 
-const MovieCard = ({movie}) => (
-	<Card fluid color='blue'>
-		<Card.Content>
-			<Image floated='left' size='small' src={movie.Poster}/>
-			<Card.Header><a href={`https://www.imdb.com/title/${movie.imdbID}`} target='_blank'>{movie.Title}</a></Card.Header>
-			<Card.Meta>
-				{movie.Year} | {printLabels(movie.Genre)} | {printLabels(movie.Director)} | {movie.Runtime} | <Icon name='star' color='yellow'/> {movie.imdbRating}
-			</Card.Meta>
-			<Card.Meta>
-				{printLabels(movie.Actors)}
-			</Card.Meta>
-			{movie.Plot}
-		</Card.Content>
-	</Card>
-);
+class MovieCard extends Component {
 
-export default MovieCard;
+	onGenreClicked = value => () => {
+		this.onLabelClicked('genre', value);
+	};
+
+	onDirectorClicked = value => () => {
+		this.onLabelClicked('director', value);
+	};
+
+	onActorClicked = value => () => {
+		this.onLabelClicked('actor', value);
+	};
+
+	onLabelClicked = (key, value) => {
+		this.props.history.push(`/${key}/${value}`);
+	};
+
+	render() {
+		const {movie} = this.props;
+		return (
+			<Card fluid color='blue'>
+				<Card.Content>
+					<Image floated='left' size='small' src={movie.Poster}/>
+					<Card.Header><a href={`https://www.imdb.com/title/${movie.imdbID}`} target='_blank'>{movie.Title}</a></Card.Header>
+					<Card.Meta>
+						{printLabels(movie.Genre, this.onGenreClicked)} | {movie.Year} | {movie.Runtime} | <Icon name='star'
+																																																		 color='yellow'/> {movie.imdbRating}
+					</Card.Meta>
+					<Card.Meta>
+						{printLabels(movie.Director, this.onDirectorClicked)} | {printLabels(movie.Actors, this.onActorClicked)}
+					</Card.Meta>
+					{movie.Plot}
+				</Card.Content>
+			</Card>
+		);
+	}
+}
+
+MovieCard.propTypes = {
+	movie: PropTypes.object.isRequired,
+	history: PropTypes.object.isRequired
+};
+
+export default withRouter(MovieCard);
