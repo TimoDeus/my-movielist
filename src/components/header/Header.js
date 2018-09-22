852357e3d7ed4f468154547f3496bac36a96b432
Timo Deuschle <github@timodeuschle.de>
2018-09-22 11:52:14 +0200

    added genre selection to menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Button, Container, Icon, Input, Menu,} from 'semantic-ui-react'
+import {Button, Container, Icon, Input, Menu, Dropdown} from 'semantic-ui-react'
 import {connect} from 'react-redux';
-import {filterByFreetext, resetFilter} from '../../actions/filter';
+import {filterByFreetext, filterByGenre, resetFilter} from '../../actions/filter';
 import * as PropTypes from 'prop-types';
 import {setSortOrder} from '../../actions/sort';
 
 const Header = props => {
-	const {filter, sort, onSetSorting} = props;
+	const {filter, sort, onSetSorting, genres, onGenreClicked} = props;
 	const hasFilter = filter.actor || filter.director || filter.genre || filter.freetext;
 	const sortOptions = [
 		{name: 'Title', title: 'Titel', asc: true},
@@ -19,6 +19,12 @@ const Header = props => {
 					<Icon name='film' style={{marginRight: '1.5em'}}/>
 					Filmliste
 				</Menu.Item>
+				{genres && <Dropdown scrolling item text='Genres'>
+					<Dropdown.Menu>
+						{genres.sort().map(genre => <Dropdown.Item key={genre} onClick={() => onGenreClicked(genre)}>{genre}</Dropdown.Item>)}
+					</Dropdown.Menu>
+				</Dropdown>
+				}
 				<Menu.Menu position='right'>
 					<Menu.Item>
 						<Input icon='search' placeholder='Suche...' onChange={props.onSearch} value={filter.freetext || ''}/>
@@ -28,7 +34,9 @@ const Header = props => {
 			<Menu text>
 				<Menu.Item header>Sortieren</Menu.Item>
 				{sortOptions.map(data =>
-					<Menu.Item key={data.name} name={data.title} active={sort.name === data.name} onClick={() => onSetSorting(data)}/>
+					<Menu.Item
+						key={data.name} name={data.title} active={sort.name === data.name}
+						onClick={() => onSetSorting(data)}/>
 				)}
 				{hasFilter && (
 					<Menu.Menu position='right'>
@@ -45,19 +53,23 @@ const Header = props => {
 const mapDispatchToProps = dispatch => ({
 	onResetFilter: () => dispatch(resetFilter()),
 	onSetSorting: data => dispatch(setSortOrder(data)),
-	onSearch: e => dispatch(filterByFreetext(e.target.value))
+	onSearch: e => dispatch(filterByFreetext(e.target.value)),
+	onGenreClicked: genre => dispatch(filterByGenre(genre))
 });
 
-const mapStateToProps = ({filter, sort}) => ({
+const mapStateToProps = ({movies, filter, sort}) => ({
 	filter,
-	sort
+	sort,
+	genres: movies.genres
 });
 
 Header.propTypes = {
 	filter: PropTypes.object.isRequired,
 	sort: PropTypes.object.isRequired,
+	genres: PropTypes.object.isRequired,
 	onResetFilter: PropTypes.func.isRequired,
 	onSetSorting: PropTypes.func.isRequired,
+	onGenreClicked: PropTypes.func.isRequired,
 	onSearch: PropTypes.func.isRequired,
 };
 
--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -2,10 +2,15 @@ import {FETCH_MOVIES} from '../actions/actionTypes';
 
 const initialState = {data: []};
 
+const processData = ({data}) => ({
+	genres: [...new Set(data.map(item => item.Genre.split(', ')).flat())],
+	data
+});
+
 const movies = (state = initialState, action) => {
 	switch (action.type) {
 		case FETCH_MOVIES:
-			return {...state, data: action.data};
+			return {...state, ...processData(action)};
 		default:
 			return state;
 	}
